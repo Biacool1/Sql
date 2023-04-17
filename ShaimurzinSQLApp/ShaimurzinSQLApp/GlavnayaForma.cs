c42e8999e6225ef3b249f4d48b30fe1a6649f15f
Biacool1 <131024112+Biacool1@users.noreply.github.com>
2023-04-17 21:48:29 +0400

    Add files via upload

---
+++ ShaimurzinSQLApp/ShaimurzinSQLApp/GlavnayaForma.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShaimurzinSQLApp
+{
+    public partial class GlavnayaForma : Form
+    {
+        public GlavnayaForma()
+        {
+            InitializeComponent();
+        }
+
+        private void Close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            RegisterForm registerform = new RegisterForm();
+            registerform.Show();
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AdminForm Form = new AdminForm();
+            Form.Show();
+        }
+    }
+}
